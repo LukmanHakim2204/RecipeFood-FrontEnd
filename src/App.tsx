41afb5a4ab6169fdf354ec1a5c60763da55c2ff6
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-01-11 23:49:23 +0700

    fitur search recipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Browse from "./pages/Browse";
+import SearchDetails from "./pages/SearchDetails";
 // import RecipeDetail from "./pages/RecipeDetail";
 // import CategoryDetails from "./pages/CategoryDetails";
-// import SearchDetails from "./pages/SearchDetails";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Browse />} />
-        {/* <Route path="/search" element={<SearchDetails />} />
-        <Route path="/recipe/:slug" element={<RecipeDetail />} />
+        <Route path="/search" element={<SearchDetails />} />
+        {/* <Route path="/recipe/:slug" element={<RecipeDetail />} />
         <Route path="/category/:slug" element={<CategoryDetails />} /> */}
       </Routes>
     </Router>
