36a48f38051d0898e5c5aa8e565b8a8048ab8a76
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-01-19 23:54:44 +0700

    link to category swapper

--- src/wrappers/CategoryWrapper.tsx
+++ src/wrappers/CategoryWrapper.tsx
@@ -3,6 +3,7 @@ import CategoryCard from "../components/CategoryCard";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Category } from "../types/type";
+import { Link } from "react-router-dom";
 
 export default function CategoryWrapper() {
     const [categories, SetCategories] = useState<Category[]>([]);
@@ -46,7 +47,9 @@ export default function CategoryWrapper() {
                 >
                     {categories.map((category) => (
                         <SwiperSlide key={category.id} className="!w-fit pb-[30px]" >
-                            <CategoryCard category={category} />
+                            <Link to={`/category/${category.slug}`}>
+                                <CategoryCard category={category} />
+                            </Link>
                         </SwiperSlide>
                     ))}
 
